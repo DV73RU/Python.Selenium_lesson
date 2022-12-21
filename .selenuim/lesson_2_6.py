ddd3c667b7f064b62d36d2df7da2bc38b31630e7
Dmitriy <dimokse@gmail.com>
2022-12-21 12:12:31 +0400

    Преоброзование в строчные тим данных

--- .selenuim/lesson_2_6.py
+++ .selenuim/lesson_2_6.py
@@ -5,9 +5,10 @@
 str_1 = "Hello"
 var_2 = "world"
 print(str_1)
+result = str_1 + ' ' + var_2
+print("Результат :" + result)  # //TODO Разобраться как работает рефакторинг
 
-
-
-
-resul = str_1+var_2
-print(resul*3)  # //TODO Разобраться как работает рефакторинг
+str_3 = 4
+var_4 = 8
+result_2 = str_3 + var_4
+print("Результат :" + str(result_2))  # Приобразовываем result_2 в строчтый тип данных
