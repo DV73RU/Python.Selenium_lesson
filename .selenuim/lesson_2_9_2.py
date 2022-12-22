4abaff9424a3d34b8c9b107673d1131f7dd5b706
Dmitriy <dimokse@gmail.com>
2022-12-22 11:53:24 +0400

    add

--- .selenuim/lesson_2_9_2.py
+++ .selenuim/lesson_2_9_2.py
@@ -2,12 +2,12 @@
 """Что такое функции и работа с ними."""
 
 
-num_1 = int(input())
-num_2 = int(input())
+# num_1 = int(input())
+# num_2 = int(input())
 
 
-def summ(num_1, num_2):     # Обявление функции включает 2 агрументов num_
+def hi(name="alex"):     # Обявление функции включает агрумент name
     """dd."""
-    result = num_1 + num_2      # Складываемм
-    print(result)  # Печать результата
-    summ(num_1, num_2)  # Запуск функции
+    print("Hello " + name)  # Печать результата
+
+    hi()
