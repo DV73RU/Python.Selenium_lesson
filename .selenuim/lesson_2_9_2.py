d3bc27038a7309116b02655d210f804ba8876aca
Dmitriy <dimokse@gmail.com>
2022-12-22 15:40:18 +0400

    add

--- .selenuim/lesson_2_9_2.py
+++ .selenuim/lesson_2_9_2.py
@@ -1,12 +1,16 @@
 # lesson_2_9_2
 """Что такое функции и работа с ними."""
 
-name = "Alex"
+name = str(input("Введити ваше имя :"))
+age = int(input("Суколько вам лет? :"))
+man = age * 12
 
 
-def his(name):     # Обявление функции включает агрумент name
+def his(name, man):     # Обявление функции включает агрумент name age
     """dd."""
-    print("Hello " + name)  # Печать результата
+    result = "Привет " + name + " тебе " + str(man) + " месяца "   # Печать результата
+    return result
 
 
-his(name)
+h = his(name, man)
+print(h)
