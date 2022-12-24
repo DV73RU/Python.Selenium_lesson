4dc586e13b6a836a483f1b2143bf281d3fa2c768
Dmitriy <dimokse@gmail.com>
2022-12-24 15:08:12 +0400

    add

---
+++ .selenuim/lesson_2_13.py
@@ -0,0 +1,8 @@
+# lesson_2_12_operator
+"""Списки."""
+
+
+personal = ["Alex", "Ivan", "Nasty", "Olga"]     # Список
+#              0      1        2        3
+result = personal[0] + " " + personal[2] 
+print(result + "Лучшая пара")  # Вызываем из списка
