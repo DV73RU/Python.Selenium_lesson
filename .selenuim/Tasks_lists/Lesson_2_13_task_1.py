6cecf7e5f66a7ac9d6939f66c0458ae07fe7448e
Dmitriy <dimokse@gmail.com>
2022-12-26 16:58:00 +0400

    Добавление в списки

---
+++ .selenuim/Tasks_lists/Lesson_2_13_task_1.py
@@ -0,0 +1,16 @@
+# lesson_2_13_задачи
+"""Дадача (списки)."""
+
+frends = ["Алексей", "Антон", "Юра", "Саша", "Данила"]
+print(frends[0])
+print(frends[1])
+print(frends[2])
+print(frends[3])
+print(frends[4])
+
+print(len(frends))  # Считаем количество элементов в списке
+element = len(frends)
+if element > 5:
+    print("Увас больше 5 друзей")
+else:
+    print("У Вас мало друзей")
