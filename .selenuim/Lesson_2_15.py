5c4e86c9ab226b1a9ab6e2d0aa7bf36cab2e7ddd
Dmitriy <dimokse@gmail.com>
2022-12-28 17:48:09 +0400

    Цикл While

--- .selenuim/Lesson_2_15.py
+++ .selenuim/Lesson_2_15.py
@@ -2,8 +2,18 @@
 """Цикл While."""
 
 
-a = 10
-while a > 1:
-    a = a - 1
-    print(a)
-   
+# a = 10
+
+# while a > 1:
+#     # a = a - 1
+#     # b = a
+#     # print(a)
+#     lists = [a-1 for i in range(a) for y in range(a-1)]
+#     print(lists)
+
+
+tmp = []
+for x in range(0, 1):
+    for y in range(0, 11):
+        tmp.append(x + y)
+print(tmp)
