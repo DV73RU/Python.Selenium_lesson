395385074b4e69090a727cc97028b8b037f51e4b
Dmitriy <dimokse@gmail.com>
2022-12-29 12:44:25 +0400

    Словари

---
+++ .selenuim/Lesson_2_17_ranga.py
@@ -0,0 +1,13 @@
+# lesson_2_17_range
+"""Range"""
+
+# range(0, 10, 2)
+# r = list(range(10))
+# x = list(range(0, 10))
+# y = list(range(0, 10, 2))
+
+for i in range(10):
+    print(i)
+# print(r)
+# print(x)
+# print(y)
---
+++ .selenuim/Lesson_2_18.py
@@ -0,0 +1,22 @@
+# lesson_2_18
+"""2.18 Работа со словарями и множествами"""
+
+# Список
+famely_1 = ["Alex", "Ivan", "Nasty", "Olga", "Semen", "Igor", "Alex"]
+print(famely_1)
+
+# Множества
+famely_2 = {"Alex", "Ivan", "Nasty", "Olga", "Semen", "Igor", "Alex"} 
+print(famely_2)
+
+
+# Словарь (ключ:значение)
+famely_3 = {"Папа": "Alex", "Мама": "Olga", "Сын": "Semen", "Доч": "Nasty"}
+# print(famely_3["Папа"])
+for k, v in famely_3.items():    # "k" - ключ, "v" - значение
+    # print(k)
+    # print(v)
+    print(f"Должность - {k}, Имя - {v}")
+ 
+
+ # Создать списки имён и должностей.
