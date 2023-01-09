4d6c4eff1861cff5d57813ddcf232418456e44fc
Dmitriy <dimokse@gmail.com>
2023-01-09 10:41:12 +0400

    add daf

--- .selenuim/strart.py
+++ .selenuim/strart.py
@@ -1,4 +1,11 @@
 """Start."""
 
 import hello
+import math
+import random
 hello.some()
+print(math.pi)
+r = random.randrange(0, 100)
+print(r)
+user = "User"
+print(user + str(random.randrange(0, 100)))
