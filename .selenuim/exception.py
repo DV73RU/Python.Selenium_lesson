ddcbb8cb86b733cd29a9ed6b65650c6bdb89e543
Dmitriy <dimokse@gmail.com>
2023-01-10 10:57:27 +0400

    except

--- .selenuim/exception.py
+++ .selenuim/exception.py
@@ -3,5 +3,16 @@
 a = int(input("Введите первое знаечение: "))
 b = int(input("Введите второе значение: "))
 
-result = int(a / b)
-print("Результат result)
+
+try:
+    result = int(a / b)
+except ZeroDivisionError:
+    result = 0
+    print("На ноль делить нельзя")
+print(f"Результат : {result}")
+print("Результат : " + str(result))
+result_2 = a + b
+print(result_2)
+# print(type(result))
+
+# ZeroDivisionError
