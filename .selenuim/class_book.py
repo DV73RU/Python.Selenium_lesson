0067104cb4b8a98057231e89c42cc84c235b6c53
Dmitriy <dimokse@gmail.com>
2023-01-12 17:58:38 +0400

    Классы из книги

---
+++ .selenuim/class_book.py
@@ -0,0 +1,15 @@
+"""Классы."""
+
+
+class Dog:
+    """Простая модель собаки."""
+
+    # Метод __init__ с тремя параметрами self, name, age.
+    def __init__(self, name, age):
+        """Инициализирует атрибуты name, age."""
+        self.name = name
+        self.name = age
+    
+    my_dog = Dog ('Bim', 4)     # Экземпляр конкретно собаки
+    print(f"Имя сойё собакти: {my_dog.name}")
+
