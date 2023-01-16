1838d2ead7140da9d1e4976a9695b8666e6b9ba6
Dmitriy <dimokse@gmail.com>
2023-01-16 12:09:12 +0400

    fix

--- .selenuim/class_car.py
+++ .selenuim/class_car.py
@@ -2,7 +2,7 @@
 
 
 class Car():
-    """Класс пользователь."""
+    """Класс автомобиль."""
 
     # Метод __init__ с четырмя параметрами first_name, last_name....
     def __init__(self, make, model, year):
@@ -10,8 +10,34 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometr = 100
 
-    def ger_desprictive_name(self):  # Добавление нового метода discribe_restarant.
+    # Добавление нового метода discribe_restarant.
+    def get_desprictive_name(self):
         """Возвращает аккуратно отформатированное описание."""
         long_name = f"{self.make} {self.model} {self.year}"
+        return long_name.title()
+
+    def update_odometr(self, miliage):
+        """Устанавливает значение одометра."""
+        if miliage >= self.odometr:
+            self.odometr = miliage
+        else:
+            print("Попытка усменьшить одометр")
+
+    def increated_odometr(self, miliage):
+        """Увеличивает значение одометра с заданным припращением."""
+        self.odometr += miliage
+
+    def read_odometr(self):
+        """Выводит значение одометра."""
+        print(f"У автомобиля пробег стостовляет: {self.odometr} мили")
+
+
+my_new_car = Car('audi', 'a4', 2019)
+my_esed
+print(my_new_car.get_desprictive_name())
+# my_new_car.odometr = 2000
+my_new_car.update_odometr(101)
+my_new_car.read_odometr()
 
