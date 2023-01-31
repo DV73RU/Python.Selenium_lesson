1e55efa7d8e9b5e3be114c9b8a6ff03f7b97160a
Dmitriy <dimokse@gmail.com>
2023-01-31 10:37:56 +0400

    add

--- Python_selenium/lesson_1.py
+++ Python_selenium/lesson_1.py
@@ -1,1 +1,20 @@
-from 
+# import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+g = Service('H:\\Python.Selenium_lesson-1\\Python_selenium\\chromedriver.exe')
+driver_g = webdriver.Chrome(options=options, service=g)
+base_url = 'https://www.saucedemo.com/'
+driver_g.get(base_url)
+driver_g.maximize_window()
+user_name = driver_g.find_element(By.ID, 'user_name')
+user_name.send_keys('standard_user')
+
+
+# time.sleep(10)
+# driver_g.close()
